6c58c35d0f0ec2ee7fbe978cb99fb606bd135437
Gabriel Nuñez <gabriel.nzn@gmail.com>
2021-05-28 15:55:38 +0100

    testing tailwind css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="w-60 h-60 rounded-full border-blue-800 ring-4 flex justify-center items-center">Hola</div>
+<div class="grid grid-container justify-center p-8 gap-8">
+  <div class="flex justify-between items-center">
+    <div class="flex justify-center items-center h-6 w-6 rounded-full ring-4 bg-green-500 ring-green-500 mx-2">
+      <span class="text-gray-100 font-semibold">1</span>
+    </div>
+    <hr class="w-52 border-2 border-green-500">
+    <div class="flex justify-center items-center h-6 w-6 rounded-full ring-4 bg-green-500 ring-green-500 mx-2">
+      <span class="text-gray-100 font-semibold">2</span>
+    </div>
+    <hr class="w-52 border-2">
+    <div class="flex justify-center items-center h-6 w-6 rounded-full ring-4 ring-gray-300 mx-2">
+      3
+    </div>
+    <hr class="w-52 border-2">
+    <div class="flex justify-center items-center h-6 w-6 rounded-full ring-4 ring-gray-300 mx-2">
+      4
+    </div>
+    <hr class="w-52 border-2">
+    <div class="flex justify-center items-center h-6 w-6 rounded-full ring-4 ring-gray-300 mx-2">
+      5
+    </div>
+  </div>
+  <div class="h-96 border-gray-400 border-4">
+  </div>
+</div>
 </template>
 
 <script lang="ts">
